6bca8054ce878e34557a2a40bb139b48fd85b903
Rafael Tiburcio <contatorafaeltiburcio@gmail.com>
2022-07-11 21:38:21 -0300

    estudo de funções

---
+++ funções.py
@@ -0,0 +1,24 @@
+# Uma das grandes utilidades das funções é para eliminar duplicações de codigos.
+# Quando trechos de codigos aparecem varias vezes realizando a mesma coisa.
+def soma (x, y):
+    return x + y
+print(soma(10, 20))
+print(soma(-20, 1010))
+
+def multiplica (x, y):
+    return x * y
+print(multiplica(4, 2))
+
+def divide (x, y):
+    return x / y
+print(divide(4, 2))
+
+
+def nome_do_seu_time ():  # essa função nao recebe nenhum parametro
+    return "vasco"
+print(nome_do_seu_time())
+
+def vazia():              
+    x = 10 + 20
+    print("o valor calculado é: ", x)
+print(vazia())
